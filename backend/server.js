f77fa87e1d1b2d7ea69c9a142d288031ddff4356
Annuuu12 <anuradhaguptajsr12@gmail.com>
2025-01-06 23:33:56 +0530

    Add initial backend routes and server setup

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+import path from 'path'
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config' 
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
+import doctorRouter from './routes/doctorRoute.js'
+import userRouter from './routes/userRoute.js'
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+
+const __dirname=path.resolve()
+
+connectDB()
+connectCloudinary()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// api endpoints
+app.use('/api/admin',adminRouter)
+app.use('/api/doctor',doctorRouter)
+app.use('/api/user',userRouter)
+
+app.use('/frontend',express.static(path.join(__dirname,"/frontend/dist")))
+
+app.use('/admin',express.static(path.join(__dirname,"/admin/dist")))
+
+app.get('/frontend/*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/frontend/dist/index.html'))
+})
+
+app.get('/admin/*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/admin/dist/index.html'))
+})
+
+app.get('*', (req, res) => {
+    res.send('Welcome to the backend server.');
+  });
+
+// app.get('/',(req,res)=>{
+//     res.send('Api Working')
+// })
+
+app.listen(port,()=>console.log("Server started",port))
